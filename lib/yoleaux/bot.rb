70d0ae93ba48d7724f6d9250fc40e8a7b61d6b42
David Kendal <dpkendal@dpk.org.uk>
2014-07-21 23:56:27 +0200

    rip the guts out!

--- lib/yoleaux/bot.rb
+++ lib/yoleaux/bot.rb
@@ -29,16 +29,13 @@ module Yoleaux
       start_processes
       send 'NICK', [], @nick
       send 'USER', [@user, '0', '*'], @realname
-    
-      @next_dispatch = Hash.new { Array.new }
+      
       @last_url = {}
-      @seendb = Database.new :seen, {}
-      @telldb = Database.new :tell, {}
-    
-      trap('INT')  { @spw.write_nonblock STOPSIG }
-      trap('TERM') { @spw.write_nonblock STOPSIG }
+      
+      trap('INT')  { stop }
+      trap('TERM') { stop }
       trap('CHLD') { @spw.write_nonblock CHLDSIG }
-    
+      
       handle_loop
     end
   
@@ -53,15 +50,11 @@ module Yoleaux
     end
   
     def handle_loop
-      @commands = {}
-      @command_ctr = 0
       loop do
         rsocks = []
         rsocks << @socket
         rsocks << @spr
-        rsocks << @scheduler.outqueue
         (rsocks << @sender.outqueue) if @sender
-        rsocks << @workers.map(&:outqueue)
         rsocks.flatten!
         selected = Queue.select(rsocks, [], [], 2)
         if selected
@@ -71,80 +64,6 @@ module Yoleaux
               line = recv
               event = parse_line line
               handle_event event
-              if command = parse_command(event)
-                if @stopasap
-                  privmsg command.channel, "#{command.user}: Sorry, I can't take any more commands because I'm about to quit."
-                else
-                  dispatch_command command
-                end
-              end
-            elsif r == @spr
-              sig = @spr.read_nonblock(1)
-              case sig
-              when STOPSIG
-                stop ENV['USER']
-              when CHLDSIG
-                while (pid, stat = ::Process.wait2(-1, ::Process::WNOHANG) rescue nil)
-                  @log.puts "stopped process: #{pid}: #{stat.exitstatus}"
-                  isworker = @workers.any? {|p| p.pid == pid }
-                  @workers.reject! {|p| p.pid == pid }
-                  if @stopasap
-                    if @workers.empty?
-                      send 'QUIT', [], "#@stopblame made me do it!"
-                      @socket.close
-                      @socket = nil
-                      return
-                    end
-                  elsif @reloading and isworker
-                    @log.puts "reloading worker #{pid} ..."
-                    start_worker
-                    @commands.each do |id, comm|
-                      if not comm.started? and not comm.done? and comm.handler_process.pid == pid
-                        dispatch_command comm
-                      end
-                    end
-                    if (@reloading.old_workers & @workers).empty?
-                      privmsg @reloading.channel, "#{@reloading.reloader}: Reload done (took #{'%.3f' % (Time.now - @reloading.started)}s)."
-                      @reloading = false
-                    end
-                  elsif stat.exitstatus.nil? or (not stat.exitstatus.zero?)
-                    if isworker
-                      @log.puts "restarting crashed worker #{pid} ..."
-                      start_worker
-                      @commands.each do |id, comm|
-                        # there's some kind of race condition-y heisenbug here, which is why we check that there is a handler before comparing pids
-                        if comm.handler_process and comm.handler_process.pid == pid
-                          if comm.started? and not comm.done?
-                            privmsg comm.channel, "#{comm.user}: Sorry, that command (#@prefix#{comm.name}) crashed." unless comm.is_a? Callback
-                            comm.done!
-                          elsif not comm.started?
-                            dispatch_command comm
-                          end
-                        end
-                      end
-                    elsif @scheduler.pid == pid
-                      @log.puts "restarting crashed schedular ..."
-                      start_scheduler
-                    elsif @sender.pid == pid
-                      @log.puts "restarting crashed sendar ..."
-                      start_sender
-                    end
-                  end
-                end
-              end
-            elsif r == @scheduler.outqueue
-              begin
-                time, task = r.receive
-              rescue EOFError
-                next
-              end
-              case task
-              when ScheduledTask
-                callback = Callback.new (@command_ctr += 1), :name => task.callback,
-                                                             :args => task.args
-                @commands[callback.id] = callback
-                dispatch callback
-              end
             elsif @sender and r == @sender.outqueue
               begin
                 @log.puts 'SENT:' + r.receive.inspect
@@ -156,19 +75,6 @@ module Yoleaux
               rescue EOFError => err
               end
               case response
-              when CommandStatus
-                command = @commands[response.command_id]
-                if response.status == :started
-                  command.started!
-                else response.status == :done
-                  command.done!
-                  if not @next_dispatch[response.command_id].empty?
-                    nxt = @next_dispatch[response.command_id].shift
-                    @next_dispatch[nxt.id] = @next_dispatch[response.command_id]
-                    @next_dispatch[response.command_id] = []
-                    dispatch nxt
-                  end
-                end
               when Message
                 command = @commands.values.select do |cmd|
                   cmd.started? and not cmd.done? and cmd.handler_process.outqueue == r
@@ -177,58 +83,10 @@ module Yoleaux
                 privmsg response.channel, command.response_prefix, response.message
               when RawMessage
                 send response.command, response.params, response.text
-              when CoreEval
-                result = (proc do
-                  command = @commands[response.command_id]
-                  begin
-                    assignments = response.vars.map {|k, v| "#{k} = response.vars[:#{k}];\n" }.join
-                    eval "#{assignments}#{response.code}"
-                  rescue Exception => e
-                    e
-                  end
-                end).call
-                inqueue = (@workers.select {|w| w.outqueue == r }.first or next).inqueue
-                inqueue.send result
-              when DatabaseAction
-                inqueue = (@workers.select {|w| w.outqueue == r }.first or next).inqueue
-                case response.action
-                when :init
-                  name, value = response.args
-                  Database.new name, value
-                  inqueue.send true
-                when :fetch_all
-                  name = response.args.first
-                  inqueue.send Database.new(name).value
-                when :fetch_key
-                  name, key = response.args
-                  inqueue.send Database.new(name).value[key]
-                when :replace
-                  name, value = response.args
-                  Database.new(name).value = value
-                  inqueue.send true
-                when :set_key
-                  name, key, value = response.args
-                  Database.new(name)[key] = value
-                  inqueue.send true
-                end
-              when ScheduledTask
-                @scheduler.inqueue.send [response.time, response]
-              when Tell
-                @telldb[Yoleaux.nick(response.to)] = @telldb[Yoleaux.nick(response.to)].to_a + [response]
               end
             end
           end
         else
-          @commands.each do |id, command|
-            if command.started? and not command.done? and (Time.now - command.started) >= @timeout
-              deal_with_runaway command
-            elsif command.done? # to prevent this process taking longer as the bot gets older
-              @log.puts "GC: reaped #{id}"
-              @commands.delete id
-            end
-          end
-          # prevent the queue from memory-leaking
-          @next_dispatch.each {|id, queue| @next_dispatch.delete(id) if queue.empty? }
           @sent_times = @sent_times[0..3]
           @last_msgs = @last_msgs[0..10]
         end
@@ -261,27 +119,6 @@ module Yoleaux
         elsif m=event.text.scan(@@uri_regexp).last
           @last_url[channel] = m.first
         end
-      
-        if not private_msg? channel, event.nick, event.text
-          @seendb[Yoleaux.nick(event.nick)] = [DateTime.now, event.nick, channel, event.text]
-        elsif time_loggable? channel
-          @seendb[Yoleaux.nick(event.nick)] = [DateTime.now, event.nick, channel]
-        end
-        if @telldb[Yoleaux.nick(event.nick)] and not @telldb[Yoleaux.nick(event.nick)].empty?
-          begin
-            # a hack to get tells to be delivered in order
-            tellcbs = @telldb[Yoleaux.nick(event.nick)].map do |tell|
-              dispatchable Callback, :name => tell.callback,
-                                     :args => tell.args,
-                                     :user => event.nick,
-                                     :channel => channel
-            end
-            @next_dispatch[tellcbs.first.id] = tellcbs[1..-1]
-            dispatch tellcbs.first
-          ensure
-            @telldb[Yoleaux.nick(event.nick)] = []
-          end
-        end
       when 'PING'
         send 'PONG', [@nick], event.text
       when 'JOIN'
@@ -295,50 +132,7 @@ module Yoleaux
         end
       end
     end
-  
-    def parse_command event
-      if event.type == 'PRIVMSG'
-        channel = (event.args[0] == @nick ? event.nick : event.args[0])
-        message = event.text
-        response_prefix = nil
-        if @privacy[channel] and @privacy[channel]['noseen_prefix'] and
-           message[0...(@privacy[channel]['noseen_prefix'].length)] == @privacy[channel]['noseen_prefix']
-          response_prefix = @privacy[channel]['noseen_prefix']
-          message = message[@privacy[channel]['noseen_prefix'].length..-1].lstrip
-        end
-      
-        if message[0...(@prefix.length)] == @prefix and
-           message[@prefix.length].to_s.match(/\A[a-z0-9]\Z/i)
-          command, args = message.split(' ', 2)
-          command = command[(@prefix.length)..-1]
-          dispatchable Command, :name => command,
-                                :args => args,
-                                :user => event.nick,
-                                :channel => channel,
-                                :last_url => @last_url[channel],
-                                :admin => @admins.include?(event.nick),
-                                :prefix => @prefix,
-                                :bot_nick => @nick,
-                                :response_prefix => response_prefix
-        end
-      end
-    end
-  
-    def dispatch_command command, id=nil
-      process = @workers[(id or command.id) % @workers.length]
-      @log.puts "#{(command.handler_process ? "moving" : "dispatching")} #{command.id} to #{process.pid}"
-      command.handler_process = nil # if we're re-dispatching this command, don't dump IOs
-      process.inqueue.send command
-      command.handler_process = process
-    end
-    alias dispatch dispatch_command
-  
-    def dispatchable klass, other
-      id = (@command_ctr += 1)
-      obj = klass.new id, other
-      @commands[id] = obj
-    end
-  
+    
     def read_config
       @config = YAML.load_file "#{BASE}/config.yaml"
       @server = (@config['server'] or raise 'no server option in config')
@@ -495,27 +289,11 @@ module Yoleaux
   
     def start_processes
       @log.puts "Starting processes ..."
-      start_workers
       start_sender
-      start_scheduler
     end
     def stop_processes
       @log.puts "Stopping processes ..."
-      stop_workers
       stop_sender
-      stop_scheduler
-    end
-  
-    def start_workers
-      @log.puts "Starting workers ..."
-      @workers = []
-      @nworkers.times do
-        start_worker
-      end
-    end
-    def stop_workers
-      @log.puts "Stopping workers ..."
-      @workers.each(&:stop)
     end
   
     def start_process kind, message, *args, &block
@@ -537,24 +315,7 @@ module Yoleaux
       @log.puts "Started #{kind}: #{pid}"
       return Worker.new(pid, inqueue, outqueue)
     end
-  
-    def start_worker
-      @workers << (start_process Worker, :loop do |process|
-        trap('INT') { }
-        trap('TERM') { process.stop }
-        require 'yoleaux/commands'
-      end)
-    end
-    def start_scheduler
-      @scheduler = start_process Scheduler, :run
-    end
-    def stop_scheduler
-      @log.puts "Stopping scheduler ..."
-      Process.kill :TERM, @scheduler.pid
-      pid, stat = Process.wait2 @scheduler.pid
-      @log.puts "scheduler (#{pid}) stopped: #{stat.exitstatus}"
-    end
-  
+    
     def start_sender
       @sender = start_process Sender, :run do |process|
         process.socket = @socket
