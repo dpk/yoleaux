0e6a4d9b5d8fd83715cc5220779743209c381b7a
David P. Kendal <dpkendal@dpk.org.uk>
2015-03-25 21:14:05 -0700

    more purging

--- lib/yoleaux/commands.rb
+++
@@ -1,8 +0,0 @@
-# this file is required by worker processes in order to load the command sets
-
-include Yoleaux::CommandSetHelper
-
-Dir.glob('./commands/*.rb').each do |command_set|
-  require command_set
-end
-
